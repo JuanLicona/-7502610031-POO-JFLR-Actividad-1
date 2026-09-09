6180c21b7543fbd558c2da3af0a78bd0f2abe72a
Juan Licona <juanfliconaromero@gmail.com>
2026-09-09 15:32:56 -0500

    Ejercicio 2 Taller 3 Completado

--- Taller 3/Ejercicio2/Intancia.java
+++ Taller 3/Ejercicio2/Intancia.java
@@ -1,4 +1,23 @@
 package Ejercicio2;
 
 public class Intancia {
+
+    static void main(String[] args) {
+
+        Matematicas.n1 = 5;
+        Matematicas.n2 = 3;
+
+
+        Matematicas.Suma();
+        System.out.println("Suma:" + Matematicas.Resultado);
+
+        Matematicas.Resta();
+        System.out.println("Resta:" + Matematicas.Resultado);
+
+        Matematicas.Multiplicacion();
+        System.out.println("Multiplicacion:" + Matematicas.Resultado);
+
+        Matematicas.Division();
+        System.out.println("Division:" + Matematicas.Resultado);
+    }
 }
--- Taller 3/Ejercicio2/Matematicas.java
+++ Taller 3/Ejercicio2/Matematicas.java
@@ -2,5 +2,26 @@ package Ejercicio2;
 
 public class Matematicas {
 
+    static int n1;
+    static int n2;
+    static int Resultado;
+
+
+public static void Suma(){
+    Resultado = n1 + n2;
+    }
+
+    public static void Resta(){
+        Resultado = n1 - n2;
+    }
+
+    public static void Multiplicacion(){
+        Resultado = n1 * n2;
+    }
+
+    public static void Division(){
+        Resultado = n1 / n2;
+    }
+
 
 }
