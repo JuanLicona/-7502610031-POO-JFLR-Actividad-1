bb66133813ed4cf0b5b1048c924d2ff6557bc1f0
Juan Licona <juanfliconaromero@gmail.com>
2026-09-11 08:24:19 -0500

    Ejecicio 3 Taller 3 Completado

---
+++ Taller 3/Ejercicio3/Deportista.java
@@ -0,0 +1,26 @@
+package Ejercicio3;
+
+public class Deportista {
+
+ public double pesoMaximo;
+
+
+ public void actualizaPesoMaximo(double nuevoPeso){
+
+  pesoMaximo = nuevoPeso;
+
+ }
+
+
+//public static void actualizaPesoMaximo(double nuevoPeso){
+
+//pesoMaximo = nuevoPeso;
+
+
+ public double getPesoMaximo() {
+  return pesoMaximo;
+ }
+ public double setPesomaximo(){
+  return pesoMaximo;
+ }
+}
---
+++ Taller 3/Ejercicio3/Instancia.java
@@ -0,0 +1,15 @@
+package Ejercicio3;
+
+public class Instancia {
+    static void main(String[] args) {
+
+Deportista D1 = new Deportista();
+
+D1.actualizaPesoMaximo(74);
+
+        System.out.println("Peso del Deportista: " + D1.getPesoMaximo());
+
+
+    }
+
+}
